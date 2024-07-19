ebb72d4ab290ffb96d7fe8086a09927fc288b4e7
Aymen <aymounadaassi@gmail.com>
2024-07-19 14:40:13 +0300

    more fixes

--- src/pages/QuizPage.vue
+++ src/pages/QuizPage.vue
@@ -1,31 +1,36 @@
 <template>
-    <div class="quizContainer">
-      <h1 class="quiz-title Raleway">Match +</h1>
-      <div class="display-container flex flex-column justify-content-center align-items-center">
-        <div class="flex flex-column justify-content-center question-header">
-          <p class="question-number Inter mb-3">Question {{ (currentQuestionIndex + 1) }} of {{ quizArray.length }} </p>
-          <p class="question Raleway mb-3">{{ currentQuestion.question }}</p>
+    <div class="quizContainer" >
+      <div class="content-container" :style="{opacity: showForm ? '1' : '0', transform: showForm ? 'translateY(0)' : 'translateY(100px)' }">
+          <h1 class="quiz-title Raleway">Match +</h1>
+          <div class="display-container flex flex-column justify-content-center align-items-center">
+            <div class="flex flex-column justify-content-center question-header">
+              <p class="question-number Inter mb-3">Question {{ (currentQuestionIndex + 1) }} of {{ quizArray.length }} </p>
+              <p class="question Raleway mb-3">{{ currentQuestion.question }}</p>
 
-        </div>
-        <div v-if="currentQuestion.options.length" class="questions-container">
-          <div v-for="(option, index) in currentQuestion.options" :key="index" :class="['Raleway','optionDes',{ 'selected-option': isSelected(option)}]">
-            <input type="radio" :id="'option' + index" name="option" :value="option" v-model="selectedOption" class="Raleway optionDes">
-            <label :for="'option' + index"  class="optionLabel">{{ option }}</label>
+            </div>
+            <div v-if="currentQuestion.options.length" class="questions-container">
+              <div v-for="(option, index) in currentQuestion.options" :key="index" :class="['Raleway','optionDes',{ 'selected-option': isSelected(option)}]">
+                <input type="radio" :id="'option' + index" name="option" :value="option" v-model="selectedOption" class="Raleway optionDes">
+                <label :for="'option' + index"  class="optionLabel">{{ option }}</label>
+              </div>
+            </div>
+            <div v-else>
+              <textarea type="text" v-model="userInput" rows="6" placeholder="Write whatever you want" class="inputText write-container">
+                </textarea>
+            </div>
+            <div class="buttons">
+              <button v-if="currentQuestionIndex > 0"  @click="previousQuestion" class="button Raleway previous-btn" >
+              Previous
+              </button>
+              <button @click="nextQuestion" class="button Raleway next-btn" :class="{ 'finish-button': isLastQuestion }">
+              {{ isLastQuestion ? 'Finish' : 'Next' }}
+              </button>
+            </div>
+
           </div>
-        </div>
-        <div v-else>
-          <input type="text" v-model="userInput" placeholder="Write whatever you want" class="inputText">
-        </div>
-        <div class="buttons">
-          <button v-if="currentQuestionIndex > 0"  @click="previousQuestion" class="button Raleway previous-btn" >
-          Previous
-          </button>
-          <button @click="nextQuestion" class="button Raleway next-btn" :class="{ 'finish-button': isLastQuestion }">
-          {{ isLastQuestion ? 'Finish' : 'Next' }}
-          </button>
-        </div>
-
       </div>
+
+
       <div class="blur-circle"></div>
       <div class="blur-circle"></div>
       <div class="films-container"></div>
@@ -38,6 +43,7 @@
   export default {
     data() {
       return {
+        showForm: false,
         currentQuestionIndex: 0,
         selectedOption: '',
         userInput: '',
@@ -46,7 +52,7 @@
           { id: "1", question: "What kind of show do you want to see?", options: ["Movie", "Series", "Anime", "Cartoon"] },
           { id: "2", question: "What show genre do you want to watch?", options: ["Action", "Reality", "Romantic", "Comedy"] },
           { id: "3", question: "What is the preferred period of the show?", options: ["Between 1 and 1.5 hours", "Between 1.5 and 2 hours", "Between 2 and 3 hours", "Over 3 hours"] },
-          { id: "4", question: "Write whatever you want", options: [] },
+          { id: "4", question: "Feel free to express your emotions, or describe your ideal movie", options: [] },
         ],
       };
     },
@@ -57,6 +63,11 @@
       isLastQuestion() {
         return this.currentQuestionIndex === this.quizArray.length - 1;
       }
+    },
+    created(){
+            setTimeout(() => {
+                this.showForm = true;
+            }, 300)
     },
     methods: {
       nextQuestion() {
@@ -73,7 +84,10 @@
         }
       },
       isSelected(option) {
-      return this.selectedOption === option;
+        if (this.selectedOption === option){
+          console.log("hello");
+        }
+        return this.selectedOption === option;
     }
     },
   };
@@ -93,12 +107,19 @@
     justify-content: center;
     align-items: center;
     flex-direction: column;
+
     /* background-image: radial-gradient(
         #8C45FF,
       black
    ); */
    background: black;
 }
+
+.content-container{
+  z-index: 1;
+  transition: all 0.3s ease-in-out;
+}
+
 .quiz-title{
   font-size: 45px;
   z-index: 1;
@@ -197,15 +218,15 @@
   
   .display-container {
     /* background-image: linear-gradient(#3c1e6b, black); */
-    background: linear-gradient(180deg, #010002 0%, #361764 50%, #361764 100%);
+    background: linear-gradient(180deg, #010002 0%, #361764 35%, #361764 50%, #361764 75%, #010002 100%);
     box-shadow: -6px 4px 10px rgba(0, 0, 0, 0.7);
     box-shadow: inset 0px 0px 6px 7px rgba(0, 0, 0, 0.45);
 
-    padding: 20px;
-    height: 80%;
-    width: 35vw;
+    padding: 30px;
+    width: 36vw;
+    height: 500px;
     max-width: 600px;
-    border-radius: 0.6em;
+    border-radius: 30px;
     z-index: 1;
     /* margin-top: 20px; */
   }
@@ -251,19 +272,60 @@
     } */
 
     .selected-option{
-      
+    background: rgb(140, 69, 255, 0.4);
+    color: #8C45FF;
     box-shadow: inset 0px 0px 6px 5px #8C45FF;
     outline: 1px solid #8C45FF;
     transition: all 0.3s ease-in-out;
     }
+
     .inputText{
-        height: 50px;
         width: 500px;
         border: solid rgb(51, 44, 3) 1px;
         background-color: #33185f;
+        text-align: left;
         color: white;
-        padding: 10px;
+        padding: 20px;
+        max-width: 100%;
+        resize:none;
+        box-shadow: inset 0px 0px 6px 1px rgba(255, 255, 255, 40%);
+        transition: all 0.3s ease-in-out;
+        outline: 1px solid rgba(255, 255, 255, 0.25);
+        outline-offset: -1px;
+        border-radius: 30px;
     }
+
+    .inputText::placeholder{
+      font-family: Raleway;
+      color: rgba(255, 255, 255, 0.47);
+    }
+    .inputText:hover{
+      
+      box-shadow: inset 0px 0px 6px 5px #8C45FF;
+      outline: 1px solid #8C45FF;
+      transition: all 0.3s ease-in-out;
+    }
+
+    .optionDes{
+    width: 80%;
+    height: 50px;
+    margin: 10px;
+    align-content: center;
+    outline: 1px solid rgba(255, 255, 255, 0.25);
+    outline-offset: -1px;
+    border-radius: 30px;
+    background: 3D3D3D;
+    color: white;
+    text-align: center;
+    box-shadow: inset 0px 0px 6px 1px rgba(255, 255, 255, 40%);
+    transition: all 0.3s ease-in-out;
+  }
+
+  .optionDes:hover{
+    box-shadow: inset 0px 0px 6px 1px rgb(255, 255, 255);
+    outline: 1px solid rgba(255, 255, 255, 0.5);
+  }
+
 .buttons {
     width: 100%;
     display: flex;
